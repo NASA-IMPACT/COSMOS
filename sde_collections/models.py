ae59ddb553563ddeeb893a7b2caa19985aebcd0e
Ashish Acharya <ashish.acharya14@gmail.com>
2023-03-28 21:04:24 -0500

    add missing fields to collection model

--- sde_collections/models.py
+++ sde_collections/models.py
@@ -2,30 +2,67 @@ from django.db import models
 from treebeard.mp_tree import MP_Node
 
 
-class Division(models.Model):
-    """Model definition for Division."""
-
-    name = models.CharField("Name", max_length=512)
-
-    class Meta:
-        """Meta definition for Division."""
-
-        verbose_name = "Division"
-        verbose_name_plural = "Divisions"
-
-    def __str__(self):
-        """Unicode representation of Division."""
-        return self.name
-
-
 class Collection(models.Model):
     """Model definition for Collection."""
+
+    class Divisions(models.IntegerChoices):
+        ASTROPHYSICS = 1, "Astrophysics"
+        BIOLOGY = 2, "Biological and Physical Sciences"
+        EARTH_SCIENCE = 3, "Earth Science"
+        HELIOPHYSICS = 4, "Heliophysics"
+        PLANETARY = 5, "Planetary Science"
+
+    class UpdateFrequencies(models.IntegerChoices):
+        DAILY = 1, "Daily"
+        WEEKLY = 2, "Weekly"
+        BIWEEKLY = 3, "Biweekly"
+        MONTHLY = 4, "Monthly"
+
+    class DocumentTypes(models.IntegerChoices):
+        IMAGES = 1, "Images"
+        DATA = 2, "Data"
+        DOCUMENTATION = 3, "Documentation"
+        SOFTWARETOOLS = 4, "Software and Tools"
+        MISSIONSINSTRUMENTS = 5, "Missions and Instruments"
+
+    class SourceChoices(models.IntegerChoices):
+        ONLY_IN_ORIGINAL = 1, "Only in original"
+        BOTH = 2, "Both"
+        ONLY_IN_SINEQUA_CONFIGS = 3, "Only in Sinequa Configs"
 
     name = models.CharField("Name", max_length=1024)
     config_folder = models.CharField("Config Folder", max_length=2048)
     url = models.URLField("URL", max_length=2048)
-    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+    division = models.IntegerField(choices=Divisions.choices)
     turned_on = models.BooleanField("Turned On", default=True)
+
+    source = models.IntegerField(choices=SourceChoices.choices)
+    update_frequency = models.IntegerField(
+        choices=UpdateFrequencies.choices, default=UpdateFrequencies.WEEKLY
+    )
+    document_type = models.IntegerField(
+        choices=DocumentTypes.choices, null=True, blank=True
+    )
+    tree_root = models.CharField("Tree Root", max_length=1024, default="")
+
+    delete = models.BooleanField(default=False)
+
+    # audit columns for production
+    audit_hierarchy = models.CharField("Audit Hierarchy", max_length=2048, default="")
+    audit_url = models.CharField("Audit URL", max_length=2048, default="")
+    audit_mapping = models.CharField("Audit Mapping", max_length=2048, default="")
+    audit_label = models.CharField("Audit Label", max_length=2048, default="")
+    audit_query = models.CharField("Audit Query", max_length=2048, default="")
+    audit_duplicate_results = models.CharField(
+        "Audit Duplicate Results", max_length=2048, default=""
+    )
+    audit_metrics = models.CharField("Audit Metrics", max_length=2048, default="")
+
+    cleaning_assigned_to = models.CharField(
+        "Cleaning Assigned To", max_length=128, default=""
+    )
+
+    notes = models.TextField("Notes", blank=True, default="")
 
     class Meta:
         """Meta definition for Collection."""
