63139fb763995b077ed62e735ef053949f7a201c
Ashish Acharya <ashish.acharya14@gmail.com>
2023-04-19 16:46:56 -0500

    add page to view candidate urls

--- sde_collections/views.py
+++ sde_collections/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from .models import Collection
+from .models import CandidateURL, Collection
 
 
 class CollectionListView(ListView):
@@ -26,6 +26,27 @@ class CollectionDetailView(DetailView):
     model = Collection
     template_name = "sde_collections/collection_detail.html"
     context_object_name = "collection"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class CandidateURLsListView(ListView):
+    """
+    Display a list of collections in the system
+    """
+
+    model = CandidateURL
+    template_name = "sde_collections/candidate_urls_list.html"
+    context_object_name = "candidate_urls"
+
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter(collection=Collection.objects.get(pk=self.kwargs["pk"]))
+        )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
