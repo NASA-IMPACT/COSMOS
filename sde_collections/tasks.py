f652b04160b2568270e142c8e9114d24bdabeb21
Carson Davis <carsonreiddavis@gmail.com>
2023-04-19 17:07:57 -0500

    update celery task to use scraper

--- sde_collections/tasks.py
+++ sde_collections/tasks.py
@@ -1,13 +1,8 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
 from config import celery_app
-from scraper.scraper.spiders.base_spider import spider_factory
+from sde_scraper.runner import run_scraper
 
 
 @celery_app.task()
-def generate_candidate_urls_async(config_folder):
+def generate_candidate_urls_async(name, url):
     """Generate candidate urls using celery."""
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(spider_factory(config_folder))
-    process.start()
+    run_scraper(name, url)
