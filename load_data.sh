36e03e71e41de3c4ab815c7433933fea663631c0
Ashish Acharya <ashish.acharya14@gmail.com>
2023-05-11 00:36:59 -0500

    add counts to load_data

--- load_data.sh
+++ load_data.sh
@@ -1,40 +1,40 @@
-python manage.py loaddata scraper/scraped_urls/algorithm_theoretical_basis_documents/urls.json
-python manage.py loaddata scraper/scraped_urls/astrogeology_analysis_ready_data/urls.json
-python manage.py loaddata scraper/scraped_urls/astromaterials_acquisition_and_curation_office/urls.json
-python manage.py loaddata scraper/scraped_urls/earth_observer_publications/urls.json
-python manage.py loaddata scraper/scraped_urls/earth_observing_dashboard/urls.json
-python manage.py loaddata scraper/scraped_urls/emac_exoplanet_modeling_and_analysis_center/urls.json
-python manage.py loaddata scraper/scraped_urls/eos_mission_page/urls.json
-python manage.py loaddata scraper/scraped_urls/exoplanet_opacities_database/urls.json
-python manage.py loaddata scraper/scraped_urls/f_prime/urls.json
-python manage.py loaddata scraper/scraped_urls/fire_information_for_resource_management_system_firms/urls.json
-python manage.py loaddata scraper/scraped_urls/gcn_circulars/urls.json
-python manage.py loaddata scraper/scraped_urls/gcn_missions_instruments_and_facilities/urls.json
-python manage.py loaddata scraper/scraped_urls/GENELAB_Publications_Website/urls.json
-python manage.py loaddata scraper/scraped_urls/general_coordinates_network_gcn/urls.json
-python manage.py loaddata scraper/scraped_urls/giss_datasets_and_derived_materials/urls.json
-python manage.py loaddata scraper/scraped_urls/giss_publication_list/urls.json
-python manage.py loaddata scraper/scraped_urls/giss_software_tools/urls.json
-python manage.py loaddata scraper/scraped_urls/goddard_institute_for_space_studies/urls.json
-python manage.py loaddata scraper/scraped_urls/heliophysics_events_knowledgebase/urls.json
-python manage.py loaddata scraper/scraped_urls/igwn_public_alerts_user_guide/urls.json
-python manage.py loaddata scraper/scraped_urls/interactive_multiinstrument_database_of_solar_flares/urls.json
-python manage.py loaddata scraper/scraped_urls/mars_exploration_program/urls.json
-python manage.py loaddata scraper/scraped_urls/mars_target_encyclopedia/urls.json
-python manage.py loaddata scraper/scraped_urls/my_nasa_data/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_carbon_monitoring_system/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_global_climate_change/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_power/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_science_earths_moon/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_science_missions_earth/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_sea_level_change/urls.json
-python manage.py loaddata scraper/scraped_urls/nasa_wavelength/urls.json
-python manage.py loaddata scraper/scraped_urls/our_changing_planet_the_view_from_space_images/urls.json
-python manage.py loaddata scraper/scraped_urls/PDS_Astropedia_Lunar_and_Planetary_Cartographic_Catalog_Website/urls.json
-python manage.py loaddata scraper/scraped_urls/PDS_Cassini_Resource_Page_Website/urls.json
-python manage.py loaddata scraper/scraped_urls/PDS_NASA_Science_Solar_System_Exploration_Website/urls.json
-python manage.py loaddata scraper/scraped_urls/pds_near_earth_asteroid_rendezvous_near_data_archive_website/urls.json
-python manage.py loaddata scraper/scraped_urls/PDS_Recently_Archived_Volumes_Website/urls.json
-python manage.py loaddata scraper/scraped_urls/PDS_Small_Bodies_Data_Ferret_Website/urls.json
-python manage.py loaddata scraper/scraped_urls/pds4_documents/urls.json
-python manage.py loaddata scraper/scraped_urls/solar_system_exploration_research_virtual_institute_sservi/urls.json
+python manage.py loaddata scraper/scraped_urls/algorithm_theoretical_basis_documents/urls.json # 125
+python manage.py loaddata scraper/scraped_urls/astrogeology_analysis_ready_data/urls.json # 29
+python manage.py loaddata scraper/scraped_urls/astromaterials_acquisition_and_curation_office/urls.json # 11594
+python manage.py loaddata scraper/scraped_urls/earth_observer_publications/urls.json # 524
+python manage.py loaddata scraper/scraped_urls/earth_observing_dashboard/urls.json # 3
+python manage.py loaddata scraper/scraped_urls/emac_exoplanet_modeling_and_analysis_center/urls.json # 282
+python manage.py loaddata scraper/scraped_urls/eos_mission_page/urls.json # 125
+python manage.py loaddata scraper/scraped_urls/exoplanet_opacities_database/urls.json # 17
+python manage.py loaddata scraper/scraped_urls/f_prime/urls.json # 43168
+python manage.py loaddata scraper/scraped_urls/fire_information_for_resource_management_system_firms/urls.json # 86
+python manage.py loaddata scraper/scraped_urls/gcn_circulars/urls.json # 61232
+python manage.py loaddata scraper/scraped_urls/gcn_missions_instruments_and_facilities/urls.json # 60653
+python manage.py loaddata scraper/scraped_urls/GENELAB_Publications_Website/urls.json # 350
+python manage.py loaddata scraper/scraped_urls/general_coordinates_network_gcn/urls.json # 60653
+python manage.py loaddata scraper/scraped_urls/giss_datasets_and_derived_materials/urls.json # 6317
+python manage.py loaddata scraper/scraped_urls/giss_publication_list/urls.json # 5803
+python manage.py loaddata scraper/scraped_urls/giss_software_tools/urls.json # 63
+python manage.py loaddata scraper/scraped_urls/goddard_institute_for_space_studies/urls.json # 1328
+python manage.py loaddata scraper/scraped_urls/heliophysics_events_knowledgebase/urls.json # 4
+python manage.py loaddata scraper/scraped_urls/igwn_public_alerts_user_guide/urls.json # 34
+python manage.py loaddata scraper/scraped_urls/interactive_multiinstrument_database_of_solar_flares/urls.json # 9
+python manage.py loaddata scraper/scraped_urls/mars_exploration_program/urls.json # 90177
+python manage.py loaddata scraper/scraped_urls/mars_target_encyclopedia/urls.json # 43377
+python manage.py loaddata scraper/scraped_urls/my_nasa_data/urls.json # 99416
+python manage.py loaddata scraper/scraped_urls/nasa_carbon_monitoring_system/urls.json # 345
+python manage.py loaddata scraper/scraped_urls/nasa_global_climate_change/urls.json # 2074
+python manage.py loaddata scraper/scraped_urls/nasa_power/urls.json # 78
+python manage.py loaddata scraper/scraped_urls/nasa_science_earths_moon/urls.json # 608
+python manage.py loaddata scraper/scraped_urls/nasa_science_missions_earth/urls.json # 18558
+python manage.py loaddata scraper/scraped_urls/nasa_sea_level_change/urls.json # 520
+python manage.py loaddata scraper/scraped_urls/nasa_wavelength/urls.json # 20521
+python manage.py loaddata scraper/scraped_urls/our_changing_planet_the_view_from_space_images/urls.json # 524
+python manage.py loaddata scraper/scraped_urls/PDS_Astropedia_Lunar_and_Planetary_Cartographic_Catalog_Website/urls.json # 98080
+python manage.py loaddata scraper/scraped_urls/PDS_Cassini_Resource_Page_Website/urls.json # 246650
+python manage.py loaddata scraper/scraped_urls/PDS_NASA_Science_Solar_System_Exploration_Website/urls.json # 225174
+python manage.py loaddata scraper/scraped_urls/pds_near_earth_asteroid_rendezvous_near_data_archive_website/urls.json # 1319
+python manage.py loaddata scraper/scraped_urls/PDS_Recently_Archived_Volumes_Website/urls.json # 246653
+python manage.py loaddata scraper/scraped_urls/PDS_Small_Bodies_Data_Ferret_Website/urls.json # 8
+python manage.py loaddata scraper/scraped_urls/pds4_documents/urls.json # 137
+python manage.py loaddata scraper/scraped_urls/solar_system_exploration_research_virtual_institute_sservi/urls.json # 9
