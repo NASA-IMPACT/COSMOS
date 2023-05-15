572a6a913a866229f457770c0bded0eefc42abe0
Ashish Acharya <ashish.acharya14@gmail.com>
2023-05-14 20:02:55 -0500

    add humanized count

--- config/settings/base.py
+++ config/settings/base.py
@@ -62,6 +62,7 @@ DJANGO_APPS = [
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
+    "django.contrib.humanize",
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
