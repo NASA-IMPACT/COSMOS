3d412cee1be2b2a39e790291a1d7510d8438b858
Ashish Acharya <ashish.acharya14@gmail.com>
2023-05-15 16:49:04 -0500

    celery now works one collection at a time

--- sde_collections/tasks.py
+++ sde_collections/tasks.py
@@ -22,7 +22,7 @@ def generate_candidate_urls_async(config_folder):
 
 
 @celery_app.task()
-def import_candidate_urls_task(collection_ids):
+def import_candidate_urls_task(collection_id):
     s3 = boto3.client(
         "s3",
         region_name="us-east-1",
@@ -30,20 +30,19 @@ def import_candidate_urls_task(collection_ids):
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     )
 
-    collections = Collection.objects.filter(id__in=collection_ids)
+    collection = Collection.objects.filter(id=collection_id)
 
-    for collection in collections:
-        collection.candidate_urls.all().delete()
-        urls_file_name = f"{collection.config_folder}_urls.json"
+    collection.candidate_urls.all().delete()
+    urls_file_name = f"{collection.config_folder}_urls.json"
 
-        try:
-            s3.download_file(
-                settings.AWS_STORAGE_BUCKET_NAME,
-                f"static/scraped_urls/{collection.config_folder}/urls.json",
-                urls_file_name,
-            )
-        except botocore.exceptions.ClientError:
-            return
+    try:
+        s3.download_file(
+            settings.AWS_STORAGE_BUCKET_NAME,
+            f"static/scraped_urls/{collection.config_folder}/urls.json",
+            urls_file_name,
+        )
+    except botocore.exceptions.ClientError:
+        return
 
-        subprocess.run(f"python manage.py loaddata {urls_file_name}", shell=True)
-        os.remove(f"{urls_file_name}")
+    subprocess.run(f"python manage.py loaddata {urls_file_name}", shell=True)
+    os.remove(f"{urls_file_name}")
