15cdba7ced07e816fd21ab23041ad4828afece49
Carson Davis <carsonreiddavis@gmail.com>
2023-05-19 14:41:36 -0500

    update generation script and move template

--- config_generation/generate_scrapers.py
+++ config_generation/generate_scrapers.py
@@ -7,12 +7,16 @@ from config_utilities import (
     open_xml_as_dict,
 )
 
+# from db_to_xml import convert_indexer_to_scraper
 
-def get_scraper_folder(scraper_folder_name="scraping_configs"):
+
+def get_or_create_folder(scraper_folder_name="scraping_configs"):
+    # TODO: probably don't need this function
     """
     scraper configs are placed in their own folder above this directory
     this gets that folder path
     """
+
     scraper_folder_path = os.path.join(
         os.path.dirname(os.getcwd()), scraper_folder_name
     )
@@ -22,10 +26,31 @@ def get_scraper_folder(scraper_folder_name="scraping_configs"):
     return scraper_folder_path
 
 
-def generate_scraper(source_name, url, scraper_template_path="scraper_template.xml"):
+def get_scraper_folder(scraper_folder_name="scraping_configs"):
     """
-    this takes the scraper template and adds the url,
-    then it creates an xml file named default.xml inside a folder named after the source
+    scraper configs are placed in their own folder above this directory
+    this gets that folder path
+    """
+
+    scraper_folder_path = os.path.join(
+        os.path.dirname(os.getcwd()), scraper_folder_name
+    )
+    # creates the folder if it doesn't already exist
+    create_folder(scraper_folder_path)
+
+    return scraper_folder_path
+
+
+def generate_brand_new_scraper(
+    source_name, url, scraper_template_path="xmls/scraper_template.xml"
+):
+    """
+
+    Args:
+        source_name (str): Collection.machine_name
+        url (str): Collection.url
+        scraper_template_path (str, optional): path to the template from which to generate the scraper.
+          Defaults to "xmls/scraper_template.xml".
     """
 
     scraper = open_xml_as_dict(scraper_template_path)
@@ -35,8 +60,13 @@ def generate_scraper(source_name, url, scraper_template_path="scraper_template.x
     create_config_folder_and_default(scraper_folder_path, source_name, scraper)
 
 
-if __name__ == "__main__":
-    from sources_to_scrape import kaylins_new_sources
+# def generate_scraper_based_on_existing(source_name, existing_config_path):
+#     existing_config_xml = get_tree(existing_config_path)
+#     scraper_xml = convert_indexer_to_scraper(existing_config_xml)
 
-    for source in kaylins_new_sources:
-        generate_scraper(source["source_name"], source["url"])
+
+# if __name__ == "__main__":
+# from sources_to_scrape import kaylins_new_sources
+
+# for source in kaylins_new_sources:
+#     generate_scraper(source["source_name"], source["url"])
