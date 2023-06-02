5dd10cee42624cc79aab46667160ad51fcdd9080
Ashish Acharya <ashish.acharya14@gmail.com>
2023-06-01 23:38:05 -0500

    update issue numbers

--- scripts/import_gh_issue_numbers.py
+++ scripts/import_gh_issue_numbers.py
@@ -1,7 +1,29 @@
-# run this in manage.py shell
 import json
 
 from sde_collections.models import Collection
+
+# run this in manage.py shell
+
+
+myjson = json.load(open("jupyter_notebooks/myjson.json"))
+
+for item in myjson:
+    collection = Collection.objects.get(name=item["guess_title"])
+    collection.github_issue_number = item["url"].split("/")[-1]
+    collection.save()
+
+# dump data
+
+Collection.objects.values_list("id", "github_issue_number")
+dict(Collection.objects.values_list("id", "github_issue_number"))
+issue_number_dict = dict(Collection.objects.values_list("id", "github_issue_number"))
+json.dump(
+    issue_number_dict,
+    open("sde_collections/fixtures/issue_numbers.json", "w"),
+    indent=4,
+)
+
+# import on server
 
 myjson = json.load(open("sde_collections/fixtures/issue_numbers.json"))
 
