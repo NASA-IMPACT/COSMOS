96e6ea9b0d9b9ddbc5776257e204fda0f01ede84
Raj Dangol <rd0077@uah.edu>
2023-06-30 12:00:24 -0500

    formatting fixes

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,7 @@ APPS_DIR = BASE_DIR / "sde_indexing_helper"
 env = environ.Env()
 
 READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
-if True:
+if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(BASE_DIR / ".env"))
 
