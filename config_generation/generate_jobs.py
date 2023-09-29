b2c81d5daf9333a9d7f525b2261b7cf04bf50d31
Carson Davis <carsonreiddavis@gmail.com>
2023-09-29 15:25:47 -0500

    refactor job generation into single class

--- config_generation/generate_jobs.py
+++ config_generation/generate_jobs.py
@@ -9,49 +9,94 @@ from db_to_xml_file_based import XmlEditor
 
 from config import collection_list, date_of_batch, n
 
-template_root_path = "xmls/"
-joblist_template_path = f"{template_root_path}joblist_template.xml"
 
-job_path_root = "../sinequa_configs/jobs/"
+class ParallelJobCreator:
+    def __init__(
+        self,
+        collection_list,
+        template_root_path="xmls/",
+        job_path_root="../sinequa_configs/jobs/",
+    ):
+        """
+        these default values rely on the old file structure, where the sinequa_configs were a
+        sub-repo of sde-indexing-helper. so when running this, you will need the sde-backend
+        code to be inside a folder called sinequa_configs
+        """
+
+        self.collection_list = collection_list
+        self.template_root_path = template_root_path
+        self.joblist_template_path = f"{template_root_path}joblist_template.xml"
+        self.job_path_root = job_path_root
+
+    def _create_job_name(self, collection_name):
+        """
+        each job that runs an individual collection needs a name based on the collection name
+        this code generates that file name as a string, and it will be passed to the function that
+            creates the actual job file
+        """
+        return f"collection.indexer.{collection_name}.xml"
+
+    def _create_joblist_name(self, index):
+        """
+        each job that runs an list of collections a name based on:
+            - the date the batch was created
+            - the index out of n total batches
+        this code generates that file name as a string, and it will be passed to the function that
+            creates the actual job file
+        """
+        return f"parallel_indexing_list-{date_of_batch}-{index}.xml"
+
+    def _create_collection_jobs(self):
+        """
+        in order to run a collection, a job must exist that runs it
+        this code:
+            - creates a job based on the job template
+            - adds the exact collection name
+            - saves it with a name that will reference the collection name
+        """
+        # create single jobs to run each collection
+        for collection in self.collection_list:
+            job = XmlEditor(f"{self.template_root_path}job_template.xml")
+            job.update_or_add_element_value("Collection", f"/SMD/{collection}/")
+            job._update_config_xml(
+                f"{self.job_path_root}{self._create_job_name(collection)}"
+            )
+
+    def make_all_parallel_jobs(self):
+        # create initial single jobs that will be referenced by the parallel job lists
+        self._create_collection_jobs()
+
+        # Create an empty list of lists
+        sublists = [[] for _ in range(n)]
+
+        # Distribute elements of the big list into sublists
+        for i in range(len(self.collection_list)):
+            # Use modulus to decide which sublist to put the item in
+            sublist_index = i % n
+            sublists[sublist_index].append(self.collection_list[i])
+
+        # create the n joblists (which will execute their contents serially in parallel
+        job_names = []
+        for index, sublist in enumerate(sublists):
+            joblist = XmlEditor(self.joblist_template_path)
+            for collection in sublist:
+                joblist.add_job_list_item(
+                    self._create_job_name(collection).replace(".xml", "")
+                )
+
+            joblist._update_config_xml(
+                f"{self.job_path_root}{self._create_joblist_name(index)}"
+            )
+            job_names.append(self._create_joblist_name(index).replace(".xml", ""))
+
+        master = XmlEditor(self.joblist_template_path)
+        master.update_or_add_element_value("RunJobsInParallel", "true")
+        [master.add_job_list_item(job_name) for job_name in job_names]
+        master._update_config_xml(
+            f"{self.job_path_root}parallel_indexing_list-{date_of_batch}-master.xml"
+        )
 
 
-def create_job_name(collection_name):
-    return f"collection.indexer.{collection_name}.xml"
-
-
-def create_joblist_name(index):
-    return f"parallel_indexing_list-{date_of_batch}-{index}.xml"
-
-
-# create single jobs to run each collection
-for collection in collection_list:
-    job = XmlEditor(f"{template_root_path}job_template.xml")
-    job.update_or_add_element_value("Collection", f"/SMD/{collection}/")
-    job._update_config_xml(f"{job_path_root}{create_job_name(collection)}")
-
-
-# Create an empty list of lists
-sublists = [[] for _ in range(n)]
-
-# Distribute elements of the big list into sublists
-for i in range(len(collection_list)):
-    # Use modulus to decide which sublist to put the item in
-    sublist_index = i % n
-    sublists[sublist_index].append(collection_list[i])
-
-# create the n joblists (which will execute their contents serially in parellel)
-job_names = []
-for index, sublist in enumerate(sublists):
-    joblist = XmlEditor(joblist_template_path)
-    for collection in sublist:
-        joblist.add_job_list_item(create_job_name(collection).replace(".xml", ""))
-
-    joblist._update_config_xml(f"{job_path_root}{create_joblist_name(index)}")
-    job_names.append(create_joblist_name(index).replace(".xml", ""))
-
-master = XmlEditor(joblist_template_path)
-master.update_or_add_element_value("RunJobsInParallel", "true")
-[master.add_job_list_item(job_name) for job_name in job_names]
-master._update_config_xml(
-    f"{job_path_root}parallel_indexing_list-{date_of_batch}-master.xml"
-)
+if __name__ == "__main__":
+    job_creator = ParallelJobCreator(collection_list=collection_list)
+    job_creator.make_all_parallel_jobs()
