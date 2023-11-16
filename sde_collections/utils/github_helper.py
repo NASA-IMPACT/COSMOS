0ec89fa1165023b3571749958a0957f8d0a6cb7e
Bishwas Praveen <bp0052@uah.edu>
2023-11-16 14:54:29 -0600

    add a hyphen between name and timestamp

--- sde_collections/utils/github_helper.py
+++ sde_collections/utils/github_helper.py
@@ -14,7 +14,7 @@ class GitHubHandler:
         self.github_token = settings.GITHUB_ACCESS_TOKEN
         self.github_repo = settings.SINEQUA_CONFIGS_GITHUB_REPO
         self.time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.github_branch = settings.GITHUB_BRANCH_FOR_WEBAPP + self.time_stamp
+        self.github_branch = settings.GITHUB_BRANCH_FOR_WEBAPP + "-" + self.time_stamp
         self.g = Github(self.github_token)
         self.repo = self.g.get_repo(f"{self.github_repo}")
         self.dev_branch = self.repo.default_branch
