36f46c216e9f07b111e7321611aa24f2c83c0e4f
Bishwas Praveen <bp0052@uah.edu>
2023-12-05 12:14:11 -0600

    configureed url for content curation request form

--- sde_indexing_helper/users/urls.py
+++ sde_indexing_helper/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from sde_indexing_helper.users.views import (
     ContactFormModelView,
+    ContentCurationRequestView,
     user_detail_view,
     user_redirect_view,
     user_update_view,
@@ -13,4 +14,9 @@ urlpatterns = [
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
     path("api/contact-us/", ContactFormModelView.as_view(), name="contact-us-api"),
+    path(
+        "api/content-curation-request/",
+        ContentCurationRequestView.as_view(),
+        name="content-curation-request-api",
+    ),
 ]
