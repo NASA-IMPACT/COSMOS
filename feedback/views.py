24279ee55b9aca2f781ddc6237fbf0e2c5a769a1
Bishwas Praveen <bp0052@uah.edu>
2024-01-23 11:23:34 -0600

    added permission classes to views

--- feedback/views.py
+++ feedback/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import ContentCurationRequest, Feedback
 from .serializers import ContentCurationRequestSerializer, FeedbackSerializer
@@ -7,8 +8,10 @@ from .serializers import ContentCurationRequestSerializer, FeedbackSerializer
 class ContactFormModelView(generics.CreateAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ContentCurationRequestView(generics.CreateAPIView):
     queryset = ContentCurationRequest.objects.all()
     serializer_class = ContentCurationRequestSerializer
+    permission_classes = [IsAuthenticated]
