d7c940c0fc7fa7fac5258bebd3c0a9dd3cb4cf99
Bishwas Praveen <bp0052@uah.edu>
2024-02-08 10:55:13 -0600

    added read_only created_field to both the forms

--- feedback/admin.py
+++ feedback/admin.py
@@ -8,6 +8,7 @@ class ContactFormAdmin(admin.ModelAdmin):
     list_display = ["name", "email", "subject", "comments", "source"]
     search_fields = ["name", "email"]
     list_filter = ["subject"]
+    readonly_fields = ["created_at"]
 
 
 @admin.register(ContentCurationRequest)
@@ -22,3 +23,4 @@ class ContentCurationRequestAdmin(admin.ModelAdmin):
     ]
     search_fields = ["name", "scientific_focus", "data_type"]
     list_filter = ["scientific_focus", "data_type"]
+    readonly_fields = ["created_at"]
