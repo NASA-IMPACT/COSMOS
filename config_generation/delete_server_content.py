d8a199ab5ace00293a6d8cd4b0cb9df030951638
Carson Davis <CarsonReidDavis@gmail.com>
2024-08-09 17:52:54 -0500

    handle engines in the deletion scripts

--- config_generation/delete_server_content.py
+++ config_generation/delete_server_content.py
@@ -2,16 +2,18 @@
 
 from db_to_xml_file_based import XmlEditor
 
-from config import batch_delete_name, collection_list, indexes_to_delete_from, source
+from config import batch_delete_name, collection_list, indexes_to_delete_from, source, engines
 
 COMMAND_FILES_PATH = "../sinequa_configs/commands/"
 DELETE_COMMAND_TEMPLATE_PATH = "xmls/delete_template.xml"
 
 command_file = XmlEditor(DELETE_COMMAND_TEMPLATE_PATH)
 
+command_file.update_or_add_element_value(element_name="Engines", element_value=",".join([engine for engine in engines]))
 for collection in collection_list:
     for index in indexes_to_delete_from:
         sql = f"delete from {index} where collection='/{source}/{collection}/'"
         command_file.update_or_add_element_value(element_name="SQL", element_value=sql, add_duplicate=True)
+
 file_name = f"{COMMAND_FILES_PATH}{batch_delete_name}.xml"
 command_file._update_config_xml(file_name)
