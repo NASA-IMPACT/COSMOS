456c76324b1f01cdc39201defaf069c0889c1725
Kiran Dawadi <kirandawadi11@gmail.com>
2024-12-19 14:11:26 -0600

    Modify serializers

--- sde_collections/serializers.py
+++ sde_collections/serializers.py
@@ -73,7 +73,7 @@ class DeltaURLSerializer(serializers.ModelSerializer):
     match_pattern_type = serializers.SerializerMethodField(read_only=True)
     delta_urls_count = serializers.SerializerMethodField(read_only=True)
     tdamm_tag = serializers.SerializerMethodField()
-    exclude_pattern_type = serializers.IntegerField(required=False)
+    exclude_pattern_type = serializers.IntegerField(read_only=True)
     include_pattern_id = serializers.IntegerField(read_only=True)
 
     def get_tdamm_tag(self, obj):
