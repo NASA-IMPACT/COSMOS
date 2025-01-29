7ed7c697d3f2e098368aa6ace70be712836dc160
Bishwas Praveen <bp0052@uah.edu>
2025-01-29 14:02:30 -0600

    added an endpoint for LRM to consume options

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import ContactFormModelView, ContentCurationRequestView
+from .views import (
+    ContactFormModelView,
+    ContentCurationRequestView,
+    FeedbackFormDropdownListView,
+)
 
 app_name = "feedback"
 urlpatterns = [
     path("contact-us-api/", ContactFormModelView.as_view(), name="contact-us-api"),
+    path(
+        "feedback-form-dropdown-options-api/",
+        FeedbackFormDropdownListView.as_view(),
+        name="feedback-form-dropdown-options-api",
+    ),
     path(
         "content-curation-request-api/",
         ContentCurationRequestView.as_view(),
