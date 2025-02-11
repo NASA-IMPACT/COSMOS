efdba33323a2c91356d9421aec5b2431bf0a0695
Kiran Dawadi <kirandawadi11@gmail.com>
2025-02-11 16:43:13 -0600

    Add homepage features and pattern application tests

--- sde_collections/tests/frontend/test_homepage_features.py
+++ sde_collections/tests/frontend/test_homepage_features.py
@@ -7,8 +7,8 @@ from ..factories import CollectionFactory
 from .base import BaseTestCase
 
 
-class TestCollections(BaseTestCase):
-    """Test collection-related functionality."""
+class TestHomepageFeatures(BaseTestCase):
+    """Test features available in COSMOS Homepage"""
 
     def setUp(self):
         """Set up test data."""
@@ -19,10 +19,10 @@ class TestCollections(BaseTestCase):
         self.collections = [CollectionFactory(curated_by=self.user) for _ in range(3)]
         self.collection_names = [collection.name for collection in self.collections]
 
-    def test_collections_display(self):
-        """Test that collections are displayed after login."""
         self.login(self.user.username, self.password)
 
+    def test_collections_display(self):
+        """Test that collections are displayed after login."""
         # Navigate to collections page
         self.driver.get(f"{self.live_server_url}/")
 
@@ -34,6 +34,23 @@ class TestCollections(BaseTestCase):
         table_text = table.text
         for collection_name in self.collection_names:
             assert collection_name in table_text, f"Collection '{collection_name}' not found in table"
+
+    def test_universal_search(self):
+        """Test universal search functionality."""
+
+        self.driver.get(f"{self.live_server_url}/")
+        # Wait for search input and enter search term
+        search_input = self.wait.until(EC.presence_of_element_located((By.ID, "collectionSearch")))
+        search_input.send_keys(self.collections[0].name)  # Search for first collection
+
+        # Wait for table to update
+        table = self.wait.until(EC.presence_of_element_located((By.ID, "collection_table")))
+
+        # Verify search results
+        table_text = table.text
+        assert self.collections[0].name in table_text, "Target collection should be present"
+        assert self.collections[1].name not in table_text, "Collection #2 should not be present"
+        assert self.collections[2].name not in table_text, "Collection #3 should not be present"
 
     def tearDown(self):
         """Clean up test data."""
