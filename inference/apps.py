4a15389158a218e5216aa750c7987bf19c53af4e
Dhanur Sharma <dhanurjsharma@gmail.com>
2025-03-20 17:24:06 -0500

    Updated process_inference_queue to run on the schedule

--- inference/apps.py
+++ inference/apps.py
@@ -5,3 +5,6 @@ class InferenceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "inference"
     verbose_name = "Inference"
+
+    def ready(self):
+        import inference.signals  # noqa F401
